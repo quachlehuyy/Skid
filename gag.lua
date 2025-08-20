986d9d84a7b82c166e91653b83fc84e96f463da4
quachlehuyy <quachlehuyy@gmail.com>
2025-08-20 23:58:08 +0700

    Update gag.lua

--- gag.lua
+++ gag.lua
@@ -541,14 +541,12 @@ end})
 Tabs.Farm:AddToggle("AutoHarvest", {Title="Auto Harvest", Default=config.AutoHarvest}):OnChanged(function(Value)
     config.AutoHarvest = Value
     SaveConfig()
-    task.spawn(function()
-        while true do
-            if Options.AutoHarvest.Value then
-            AutoCollect()
-            end
-            task.wait(0.1)
-        end
     end)
+task.spawn(function()
+    while Options.AutoHarvest.Value do
+        AutoCollect()
+    end
+    task.wait(0.1)
 end)
 
 
